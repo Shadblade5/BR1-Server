bc82b61024148c89f5289523462029fa370e0336
Frank <randomcash55@yahoo.com>
2021-02-11 15:49:11 -0500

    Added function to retrieve the Server Group from a name and client from sgid.

--- teamspeak.js
+++ teamspeak.js
@@ -19,43 +19,21 @@ const teamspeak = new TeamSpeak({
   nickname: "ServerMonitor"
 });
 
-
-
-/*
-TeamSpeak.connect({
-  host: hostIP,
-  queryport: qPort,
-  serverport: sPort,
-  protocol: QueryProtocol.RAW,
-  username: "BR1Teamspeak",
-  password: "xJUE7Mue",
-  nickname: "ServerMonitor"
-}).then(async teamspeak => {
-
-  teamspeak.on("close", async () => {
-    console.log("disconnected, trying to reconnect...")
-    await teamspeak.reconnect(-1, 1000)
-    console.log("reconnected!")
-  })
-
-})
-*/
+const clientListFromSGID = async (sgid) => {
+  return teamspeak.serverGroupClientList(sgid);
+}
 
 teamspeak.on("ready", async () => {
   console.log(`Ready on ${hostIP}:${sPort}`);
-  const perms = await teamspeak.clientDbList();
-  perms.forEach(client => {
-    console.log(`${client.clientNickname}: ${client.clientUniqueIdentifier}`)
-  })
+  console.log(await teamspeak.getServerGroupByName("Leadership Cert"));
+  // const serverGroup = await teamspeak.serverGroupList();
+  // const clientList = await teamspeak.serverGroupClientList(116);
+  // console.log(clientList);
 });
 
 teamspeak.on("error", e => {
   console.log(e)
 });
-
-// teamspeak.whoami().then(whoami => {
-//   console.log(whoami);
-// })
 
 // teamspeak.on("textmessage", async txt => {
 //   if(!txt.invoker.isQuery()){
