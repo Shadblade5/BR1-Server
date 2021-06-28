217c3021e4fded84a69eb94d723e487bb8c666d6
Shadblade5 <shadblade8@gmail.com>
2021-06-28 03:02:35 -0400

    Starting teamspeak integration

--- teamspeak.js
+++ teamspeak.js
@@ -1,11 +1,12 @@
 //import { TeamSpeak, QueryProtocol } from "./ts3-nodejs-library/src/TeamSpeak"
-const {TeamSpeak} = require("ts3-nodejs-library");
+const {TeamSpeak, QueryProtocol} = require("ts3-nodejs-library");
 
-const mysql = require('mysql');
+const config = require('./config.json')
+//const mysql = require('mysql');
 require('dotenv').config();
-const SQLPASS = process.env.SQLPASS
-const TS3PASS = process.env.TS3PASS
-const hostIP = process.env.hostIP
+const TS3PASS = config.TS3PASS
+const hostIP = config.TS3IP
+
 
 const qPort = 10011;
 const sPort = 9987;
@@ -14,10 +15,15 @@ const teamspeak = new TeamSpeak({
   host: hostIP,
   queryport: qPort,
   serverport: sPort,
+  protocol: QueryProtocol.RAW,
   username: "BR1Teamspeak",
   password: TS3PASS,
   nickname: "ServerMonitor"
 });
+
+
+var shadts = 'Grd6QMFVyHf7pPFGsqazWFt8PK8='
+
 
 const clientListFromSGID = async (sgid) => {
   return teamspeak.serverGroupClientList(sgid);
@@ -25,19 +31,29 @@ const clientListFromSGID = async (sgid) => {
 
 teamspeak.on("ready", async () => {
   console.log(`Ready on ${hostIP}:${sPort}`);
-  console.log(await teamspeak.getServerGroupByName("Leadership Cert"));
+/*  var servergroup = await teamspeak.getServerGroupByName("Leadership Cert");
+  //console.log(servergroup);
   // const serverGroup = await teamspeak.serverGroupList();
-  // const clientList = await teamspeak.serverGroupClientList(116);
-  // console.log(clientList);
+   var clientList = await teamspeak.serverGroupClientList(servergroup);
+
+  // console.log(clientList);*/
 });
 
 teamspeak.on("error", e => {
   console.log(e)
 });
 
-// teamspeak.on("textmessage", async txt => {
-//   if(!txt.invoker.isQuery()){
-//     //teamspeak.sendTextMessage(txt.channelGroupId, 2, "Got That!");
-//     console.log(`${txt.invoker.nickname}: "${txt.msg}"`);
-//   }
-// })
+async function addClientToServerGroup(ts3uid,sgid)
+{
+ try{
+  var client = await teamspeak.getClientByUid(ts3uid)
+  await client.addGroups(sgid)
+ }catch(e)
+ {
+   console.log(`\n${e}`)
+ }
+}
+
+module.exports = {
+addClientToServerGroup
+};
