7916de68de9a694bc21b63006c07d8f9c205c740
Shadblade5 <shadblade8@gmail.com>
2022-01-28 17:17:46 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 const loadCommands = require('./load-commands')
-const { prefix } = require('../../config.json')
+const { prefix } = require('../../../config.json')
 
 module.exports = {
   commands: ['help', 'h'],
